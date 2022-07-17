b404ff4a4f0b47e2218c4763a508eb06571b83d6
mohammad rezaii <rezaiimohammad003@gmail.com>
2022-07-17 12:11:06 +0430

    add tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-red-500 text-4xl text-center">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
